7d8f6da3689be1dddef0c7f4decdffff462b84e3
Berz <dasberz@outlook.com>
2021-11-17 21:05:27 +0100

    ejs

--- src/App.js
+++ src/App.js
@@ -32,6 +32,10 @@ app.get("/",(req,res) => {
    res.render("index");
 });
 
+app.get("./login", (req, res) => {
+    res.render("./Login");
+});
+
 app.listen(port, () => {
     console.log('Server is running at port ' + port);
 });
